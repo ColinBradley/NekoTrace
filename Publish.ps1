a220894179d2250458db23d5be97f7a6c6ea05d4
Colin Bradley <colin@colinbradley.co.uk>
2025-09-21 16:19:13 +0100

    publish.yml - Adding docker publishing

--- Publish.ps1
+++ Publish.ps1
@@ -3,14 +3,14 @@ $ErrorActionPreference = "Stop"
 $version = (Select-Xml -Path .\NekoTrace.Web\NekoTrace.Web.csproj -XPath /Project/PropertyGroup/Version).Node.InnerText
 $namePrefix = "NekoTrace-${version}-"
 
-if (Test-Path Publish) {
-    Remove-Item Publish/* -Recurse
+if (Test-Path publish) {
+    Remove-Item publish/* -Recurse
 } else {
-    New-Item Publish -ItemType Directory | Out-Null
+    New-Item publish -ItemType Directory | Out-Null
 }
 
 foreach ($publishName in @("Portable", "Linux64SelfContained", "Win64", "Win64SelfContained")) {
     dotnet publish ./NekoTrace.Web/NekoTrace.Web.csproj -p:PublishProfile=$publishName
 
-    Compress-Archive -Path "./NekoTrace.Web/bin/Release/net9.0/publish/${publishName}/*" -DestinationPath "./Publish/${namePrefix}${publishName}.zip"
+    Compress-Archive -Path "./NekoTrace.Web/bin/Release/net9.0/publish/${publishName}/*" -DestinationPath "./publish/${namePrefix}${publishName}.zip"
 }
